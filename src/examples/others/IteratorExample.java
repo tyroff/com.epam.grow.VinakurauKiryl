1bfb5e425d5b58e5b6ad2dbac4b76fbec2f48791
Kiryl Vinakurau <Kiryl_Vinakurau@epam.com>
2021-03-18 22:52:42 +0300

    Exsample: Iterator and ListIterator is was created

---
+++ src/examples/others/IteratorExample.java
@@ -0,0 +1,21 @@
+package examples.others;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorExample {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Germany");
+        list.add("France");
+        list.add("Italy");
+        list.add("Spain");
+
+        Iterator<String> iterator = list.iterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
---
+++ src/examples/others/ListIteratorExample.java
@@ -0,0 +1,22 @@
+package examples.others;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class ListIteratorExample {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("Germany");
+        list.add("France");
+        list.add("Italy");
+        list.add("Spain");
+
+        ListIterator<String> listIterator = list.listIterator();
+
+        while(listIterator.hasNext()) {
+            System.out.println(listIterator.next());
+        }
+    }
+}
