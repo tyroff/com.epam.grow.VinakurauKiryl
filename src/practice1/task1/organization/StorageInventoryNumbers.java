5a4e48ebfa7ee0a20c23d94886e3af2cfbbc3516
Kiryl Vinakurau <Kiryl_Vinakurau@epam.com>
2021-03-24 13:51:25 +0300

    Add commit in public final class StorageInventoryNumbers

--- src/practice1/task1/organization/StorageInventoryNumbers.java
+++ src/practice1/task1/organization/StorageInventoryNumbers.java
@@ -36,21 +36,38 @@ public final class StorageInventoryNumbers {
     }
 
     /**
-     * 
-     * @param inventoryNumber
+     * Deletes an object of type {@code <T extends InventoryEntity>} to the {@code Map<Integer, Object> storageInventoryNumbers}
+     * by key {@code inventoryNumber}.
+     * @param inventoryNumber key of an {@code Integer} value.
      */
     public void deleteByInventoryNumber(Integer inventoryNumber) {
         storageInventoryNumbers.remove(inventoryNumber);
     }
 
+    /**
+     * Updates an object of type {@code <T extends InventoryEntity>} to the {@code Map<Integer, Object> storageInventoryNumbers}
+     * by key {@code inventoryNumber}. The value key does not change.
+     * @param inventoryNumber key of an {@code Integer} value.
+     * @param entity it is object of type {@code <T extends InventoryEntity>}.
+     */
     public <T extends InventoryEntity> void updateByInventoryNumber(Integer inventoryNumber, T entity) {
         storageInventoryNumbers.put(inventoryNumber, entity);
     }
 
+    /**
+     * Finds an object of type {@code <T extends InventoryEntity>} to the {@code Map<Integer, Object> storageInventoryNumbers}
+     * by key {@code inventoryNumber}.
+     * @param inventoryNumber key of an {@code Integer} value.
+     * @return object of type {@code <T extends InventoryEntity>}.
+     */
     public <T extends InventoryEntity> T readByInventoryNumber(Integer inventoryNumber) {
         return (T) storageInventoryNumbers.get(inventoryNumber);
     }
 
+    /**
+     * Finds all objects of type {@code <T extends InventoryEntity>} to the {@code Map<Integer, Object> storageInventoryNumbers}.
+     * @return a map with all keys and values.
+     */
     public Map<Integer, Object> readAll() {
         return storageInventoryNumbers;
     }
@@ -62,6 +79,7 @@ public final class StorageInventoryNumbers {
         StorageInventoryNumbers that = (StorageInventoryNumbers) o;
         return Objects.equals(storageInventoryNumbers, that.storageInventoryNumbers);
     }
+
 
     @Override
     public int hashCode() {
