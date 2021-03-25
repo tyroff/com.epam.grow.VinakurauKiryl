bb486098127e7f09d61150c1cb48fb953d98b64a
Kiryl Vinakurau <Kiryl_Vinakurau@epam.com>
2021-03-25 10:40:15 +0300

    Practice 1 Task 1: Main add Organization

--- src/practice1/task1/Main.java
+++ src/practice1/task1/Main.java
@@ -17,8 +17,6 @@ public class Main {
         Address physicalAddress = new Address(Country.BELARUS, "Vitebsk", "Tereshkovoj", 7, 0);
         Organization organization = new Organization("Bus Deport #1", legalAddress, physicalAddress, 13);
 
-
-        
         Transport transport1 = new WheeledTransport("MAZ", "206", FuelType.DIESEL_FUEL, 72, 90, 4, "Republic of Belarus", 1000000, 15.3);
         Transport transport2 = new WheeledTransport("MAZ", "216", FuelType.DIESEL_FUEL, 88, 80, 4, "Republic of Belarus", 999000, 14.8);
         Transport transport3 = new WheeledTransport("MAZ", "226", FuelType.ELECTRICITY, 69, 110, 4, "Republic of Belarus", 1000002, 22.1);
@@ -26,8 +24,7 @@ public class Main {
         Transport transport5 = new WheeledTransport("MAZ", "246", FuelType.GASOLINE, 101, 78, 4, "Republic of Belarus", 1000900, 5.3);
         Transport transport6 = new WheeledTransport("MAZ", "256", FuelType.ELECTRICITY, 55, 120, 6, "Republic of Belarus", 800070, 7.7);
         Transport transport7 = new WheeledTransport("MAZ", "266", FuelType.COMPRESSED_NATURAL_GAS, 150, 90, 8, "Republic of Belarus", 700070, 18.9);
-
-
+        
         organization.getStorageInventoryNumbers().add(transport1);
         organization.getStorageInventoryNumbers().add(transport2);
         organization.getStorageInventoryNumbers().add(transport3);
