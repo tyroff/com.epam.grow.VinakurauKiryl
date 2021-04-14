bdb65e715c2c48bcdcee847c7b83efc7a71e9b66
Kiryl Vinakurau <Kiryl_Vinakurau@epam.com>
2021-04-14 15:54:04 +0300

    Create Practice 3 Task 3 FastFileMover

--- src/practice3/task3_FastFileMover/FastFileMover.java
+++ src/practice3/task3_FastFileMover/FastFileMover.java
@@ -1,6 +1,10 @@
 package practice3.task3_FastFileMover;
 
 import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public final class FastFileMover {
 
@@ -45,6 +49,44 @@ public final class FastFileMover {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        readPath.delete();
+    }
+
+    /**
+     * moves a file from one directory to another directory. The entrance takes both paths. Using FileChannel.
+     *
+     * @param readPath  the path of the file to be moved.
+     * @param writePath the path where you want to move the file.
+     */
+    public static void movingDerectoryFileChannel(File readPath, File writePath) {
+        try {
+            FileChannel source = new FileInputStream(new File(readPath.getPath())).getChannel();
+            FileChannel desti = new FileOutputStream(new File(writePath.getPath())).getChannel();
+            desti.transferFrom(source, 0, source.size());
+            source.close();
+            desti.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File " + e + " not found.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        readPath.delete();
+    }
+
+    /**
+     * moves a file from one directory to another directory. The entrance takes both paths. Using NIO2.
+     *
+     * @param readPath  the path of the file to be moved.
+     * @param writePath the path where you want to move the file.
+     */
+    public static void movingDerectoryNIO2(File readPath, File writePath) {
+        try {
+            Files.move(Paths.get(readPath.getPath()), Paths.get(writePath.getPath()), StandardCopyOption.ATOMIC_MOVE,
+                    StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
         readPath.delete();
     }
--- src/practice3/task3_FastFileMover/Main.java
+++ src/practice3/task3_FastFileMover/Main.java
@@ -1,24 +1,21 @@
 package practice3.task3_FastFileMover;
 
 import java.io.File;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-/*
         Scanner scanner = new Scanner(System.in);
         System.out.print("Write the path of the file to read (for example enter 'd:\\temp\\text.txt'");
         String pathRead = scanner.nextLine();
         File pathReadFile = new File(pathRead);
-*/
-        File pathReadFile = new File("d:\\move.txt");
-/*
         System.out.print("Write the path of the directory to write (for example enter 'd:\\'");
         String pathWrite = scanner.nextLine();
         File pathWriteFile = new File(pathWrite);
-*/
-        File pathWriteFile = new File("d:\\temp\\move.txt");
-//        scanner.close();
-//        FastFileMover.movingDirectory(pathReadFile, pathWriteFile);
-        FastFileMover.movingDirectoryUsingBuffer(pathReadFile, pathWriteFile);
+        FastFileMover.movingDirectory(pathReadFile, pathWriteFile);
+//        FastFileMover.movingDirectoryUsingBuffer(pathReadFile, pathWriteFile);
+//        FastFileMover.movingDerectoryFileChannel(pathReadFile, pathWriteFile);
+//        FastFileMover.movingDerectoryNIO2(pathReadFile, pathWriteFile);
+        scanner.close();
     }
 }
