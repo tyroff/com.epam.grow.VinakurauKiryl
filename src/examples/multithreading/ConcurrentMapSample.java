9c9acda81aa8256e4d6b65a2f58c67344b6a82ad
Kiryl Vinakurau <Kiryl_Vinakurau@epam.com>
2021-04-22 15:20:01 +0300

    Multithreding ConcurrentHashMap of sample is created

---
+++ src/examples/multithreading/ConcurrentMapSample.java
@@ -0,0 +1,46 @@
+package examples.multithreading;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ConcurrentMapSample {
+
+    public static void main(String[] args) {
+        Map<Integer, Integer> resource = new ConcurrentHashMap<>();
+        List<Thread> pool = new ArrayList<>();
+
+        for (int i = 0; i < 10_000; i++) {
+            final int val = i;
+            Thread t = new Thread(() -> {
+                try {
+                    Thread.sleep(ThreadLocalRandom.current().nextInt(100));
+                    resource.put(val, val + ((int) (Math.random() * 10)));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            pool.add(t);
+            t.setName("Thread #" + val);
+        }
+
+        long startTime = System.currentTimeMillis();
+
+        pool.forEach(e -> e.start());
+        pool.forEach(e -> {
+            try {
+                e.join();
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
+        });
+
+        System.out.println(System.currentTimeMillis() - startTime);
+
+        System.out.println("Size is: " + resource.size());
+
+    }
+}
