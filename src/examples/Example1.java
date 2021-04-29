1c65e689166e3f73f1ce097b2e06bedeb161c1d3
Kiryl Vinakurau <Kiryl_Vinakurau@epam.com>
2021-04-29 11:34:51 +0300

    Example1 deleted

--- src/examples/Example1.java
+++
@@ -1,8 +0,0 @@
-import java.util.concurrent.ConcurrentHashMap;
-
-public class Example1 {
-    public void printAverage(int totalSum, int totalNumber) {
-        int average = totalSum + totalNumber;
-
-    }
-}
