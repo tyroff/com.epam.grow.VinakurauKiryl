4520dd0ea575356a8e23cb124604232dd34c155b
Kiryl Vinakurau <Kiryl_Vinakurau@epam.com>
2021-04-30 12:44:55 +0300

    Create Calculation

---
+++ src/practice5/task2_TestCrazyCalculator/Calculator.java
@@ -0,0 +1,141 @@
+package practice5.task2_TestCrazyCalculator;
+
+/**
+ * Performs the following operations on integer and double numbers: addition, subtraction, multiplication, division,
+ * root / power function.
+ *
+ * @author Kiryl_Vinakurau
+ */
+public class Calculator {
+
+    /**
+     * Adds two numbers of type {@code int}.
+     *
+     * @param i1 number type int.
+     * @param i2 number type int.
+     * @return addition of two numbers of type int.
+     */
+    public static int addition(int i1, int i2) {
+        return i1 + i2;
+    }
+
+    /**
+     * Adds two numbers of type {@code double}.
+     *
+     * @param i1 number type double.
+     * @param i2 number type double.
+     * @return addition of two numbers of type double.
+     */
+    public static double addition(double i1, double i2) {
+        return i1 + i2;
+    }
+
+    /**
+     * Subtraction two numbers of type {@code int}.
+     *
+     * @param i1 number type double.
+     * @param i2 number type double.
+     * @return subtraction of two numbers of type double.
+     */
+    public static int subtraction(int i1, int i2) {
+        return i1 - i2;
+    }
+
+    /**
+     * Subtraction two numbers of type {@code double}.
+     *
+     * @param i1 number type double.
+     * @param i2 number type double.
+     * @return subtraction of two numbers of type double.
+     */
+    public static double subtraction(double i1, double i2) {
+        return i1 - i2;
+    }
+
+    /**
+     * Multiplication two numbers of type {@code int}.
+     *
+     * @param i1 number type int.
+     * @param i2 number type int.
+     * @return multiplication of two numbers of type int.
+     */
+    public static int multiplication(int i1, int i2) {
+        return i1 * i2;
+    }
+
+    /**
+     * Multiplication two numbers of type {@code double}.
+     *
+     * @param i1 number type double.
+     * @param i2 number type double.
+     * @return multiplication of two numbers of type double.
+     */
+    public static double multiplication(double i1, double i2) {
+        return i1 * i2;
+    }
+
+    /**
+     * Division two numbers of type {@code int}.
+     *
+     * @param i1 number type int.
+     * @param i2 number type int.
+     * @return division of two numbers of type int.
+     */
+    public static int division(int i1, int i2) {
+        return i1 * i2;
+    }
+
+    /**
+     * Division two numbers of type {@code double}.
+     *
+     * @param i1 number type double.
+     * @param i2 number type double.
+     * @return division of two numbers of type double.
+     */
+    public static double division(double i1, double i2) {
+        return i1 * i2;
+    }
+
+    /**
+     * Root number of type {@code int}.
+     *
+     * @param i1 number type int.
+     * @return root number of type int.
+     */
+    public static double root(int i1) {
+        return Math.sqrt(i1);
+    }
+
+    /**
+     * Root number of type {@code double}.
+     *
+     * @param i1 number type double.
+     * @return root number of type double.
+     */
+    public static double division(double i1) {
+        return Math.sqrt(i1);
+    }
+
+    /**
+     * Power function number of type {@code int}.
+     *
+     * @param i1 number type int.
+     * @param powValue power type int.
+     * @return root number of type int.
+     */
+    public static double pow(int i1, int powValue) {
+        return Math.pow(i1, powValue);
+    }
+
+    /**
+     * Power function number of type {@code double}.
+     *
+     * @param i1 number type double.
+     * @param powValue power type int.
+     * @return root number of type double.
+     */
+    public static double pow(double i1, int powValue) {
+        return Math.pow(i1, powValue);
+    }
+
+}
