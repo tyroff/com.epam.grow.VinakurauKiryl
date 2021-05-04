d1c887a0c695fdb575818984e3c3d442cf950e25
Kiryl Vinakurau <Kiryl_Vinakurau@epam.com>
2021-05-04 16:44:56 +0300

    Daemon Thread created

---
+++ src/examples/multithreading/DaemonSample.java
@@ -0,0 +1,66 @@
+package examples.multithreading;
+
+import static java.lang.Thread.sleep;
+
+public class DaemonSample {
+    public static void main(String[] args) {
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+/*                while (!isInterrupted()) {
+                    try {
+                        sleep(1_000);
+                        System.out.println("Thread is work!");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        interrupt(); // т.к. это ошибка sleep(), по этой причине необходимо в catch завершить поток.
+                        Правильная констркуция описана ниже, в try помещён цикл с методом isInterrupt().
+                        //break; - в данной конструкции так тоже можно завершить поток.
+                    }
+                }*/
+                try {
+                    while (!isInterrupted()) {
+                        sleep(1_000);
+                        System.out.println("Thread is work!");
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println("----------Thread interrupted-----------");
+                }
+            }
+        };
+
+        Thread daemonThread = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        sleep(1_000);
+                        System.out.println("I am daemon! HA-HA-HA :)");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        thread.start();
+        daemonThread.setDaemon(true);
+        daemonThread.start();
+        try {
+            sleep(5_000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread.interrupt();
+
+        try {
+            while (true) {
+                sleep(1_000);
+                System.out.println("main");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main interrupted");
+        }
+    }
+}
