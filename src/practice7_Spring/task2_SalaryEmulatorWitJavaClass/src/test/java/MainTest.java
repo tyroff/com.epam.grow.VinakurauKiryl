ba1be511b9ea8ba8b233e484169ac8c7a32f62d6
Kiryl Vinakurau <Kiryl_Vinakurau@epam.com>
2021-06-01 12:49:01 +0300

    Create

--- src/practice7_Spring/task2_SalaryEmulatorWitJavaClass/src/test/java/MainTest.java
+++
@@ -1,7 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({EmployeeServiceTest.class, PositionServiceTest.class, SalaryServiceTest.class})
-public class MainTest {
-}
