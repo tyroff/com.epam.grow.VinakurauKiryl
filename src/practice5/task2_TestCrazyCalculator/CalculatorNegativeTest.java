30e0a01d55600149b2e0d2b9c620329e7b9df6a9
Kiryl Vinakurau <Kiryl_Vinakurau@epam.com>
2021-06-30 16:39:39 +0300

    ADD

--- src/practice5/task2_TestCrazyCalculator/CalculatorNegativeTest.java
+++ src/practice5/task2_TestCrazyCalculator/CalculatorNegativeTest.java
@@ -4,10 +4,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
-
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertTrue;
 
 public class CalculatorNegativeTest {
 
